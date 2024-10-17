35234e6bb570a70d1fabee6b4b8683fc7c12524e
xden78 <venera.opt@gmail.com>
2024-10-17 16:37:20 +0300

    attempt 1

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        // Print chars :'c', 'h', 'a', 'r' using int representation. I
+        int c = 99;
+        int h = 104;
+        int a = 97;
+        int r = 114;
+        System.out.println((char)c);
+        System.out.println((char)h);
+        System.out.println((char)a);
+        System.out.println((char)r);
     }
 }
